e383afc7314056d2c40cc802a0f5dac6fcc0fe3f
Sergey <Nikolaychuk29@mail.ru>
2023-02-03 23:15:23 +0300

    Homwork_lesson 2

---
+++ 020/Program.cs
@@ -0,0 +1,18 @@
+﻿// 20. Дано число. Проверить кратно ли оно 7 и 23
+
+
+string number = Console.ReadLine();
+
+int number1 = Convert.ToInt32(number);
+
+if((number1 % 7 == 0) && (number1 % 23 == 0)){
+
+    Console.WriteLine(true);
+
+} else{
+    Console.WriteLine(false);
+}
+
+
+
+
---
+++ 022/Program.cs
@@ -0,0 +1,11 @@
+﻿// 22.По двум заданным числам проверять является ли одно квадратом другого.
+
+string a= Console.ReadLine();
+string b= Console.ReadLine();
+int a1 = Convert.ToInt32(a);
+int b1= Convert.ToInt32(b);
+if (a1*a1== b1 || b1*b1==a1){
+    Console.WriteLine(true);
+}else {
+    Console.WriteLine(false);
+}
